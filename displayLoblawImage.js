2442f91ebccf02a20104b4d040958c4a1602aaaa
jsdu <jsdu@uwaterloo.ca>
2015-07-10 09:15:08 -0400

    Added animations in background and added support for sobeys and metro

--- displayLoblawImage.js
+++ displayLoblawImage.js
@@ -2,10 +2,17 @@ var loblawImages = ['http://i.imgur.com/QPlnkeC.jpg',
  'http://i.imgur.com/PH7PiKE.jpg', 'http://i.imgur.com/X3GEAaN.jpg',
  'http://i.imgur.com/hxWMekc.jpg','http://i.imgur.com/h6j2vmf.jpg',
  'http://i.imgur.com/MojVVpH.jpg','http://i.imgur.com/5kBV4pL.jpg',
- 'http://i.imgur.com/YQgGk6R.jpg',];
+ 'http://i.imgur.com/YQgGk6R.jpg','http://i.imgur.com/YQgGk6R.jpg',];
 var images = $('img');
 for (i = 0; i < images.length; i++){
 	var displayImage = loblawImages[Math.floor(Math.random() * loblawImages.length)];
 	var img = images[i];
 	$(img).attr('src', displayImage); 
 }
+
+document.body.style.backgroundImage = "url('http://i.imgur.com/ruowh3r.gif')";
+
+
+$("body").children().each(function () {
+    $(this).html( $(this).html().replace(/Walmart/g,"Loblaws").replace(/Wal-Mart/g,"Loblaws").replace(/Metro/g,"Loblaws").replace(/Sobeys/g,"Loblaws") );
+});
